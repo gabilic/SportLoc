c67b2a0aaf2356e2129f596e61ae5d29ebb3ec08
gabilic <gabriel.ilic@gmail.com>
2018-10-31 01:12:44 +0100

    Created initial MVVM structure
    The project is organised in a way that follows Model-View-ViewModel
    architectural pattern. Considering that in Git it's not possible to
    commit empty folders, special '.gitkeep' files are added as placeholders to every empty folder.

--- app/src/main/java/hr/foi/air/sportloc/view/ui/activity/MainActivity.java
+++ app/src/main/java/hr/foi/air/sportloc/view/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package hr.foi.air.sportloc;
+package hr.foi.air.sportloc.view.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import hr.foi.air.sportloc.R;
 
 public class MainActivity extends AppCompatActivity {
 
