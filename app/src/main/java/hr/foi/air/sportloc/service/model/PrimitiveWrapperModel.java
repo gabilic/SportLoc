56c32088411b7b429e08801b48eaa684db31a805
gabilic <gabriel.ilic@gmail.com>
2018-12-08 03:19:16 +0100

    Created Web service communication logic for LoginActivity
    Implemented the logic for communication with the Web service for LoginActivity using Retrofit REST Client and Gson data serialization converter.

---
+++ app/src/main/java/hr/foi/air/sportloc/service/model/PrimitiveWrapperModel.java
@@ -0,0 +1,16 @@
+package hr.foi.air.sportloc.service.model;
+
+public class PrimitiveWrapperModel {
+    private int userId;
+
+    public PrimitiveWrapperModel() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
