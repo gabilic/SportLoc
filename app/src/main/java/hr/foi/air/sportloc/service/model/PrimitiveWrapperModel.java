1d649be47a8038713692f4f4b8e7ca45351209fa
gabilic <gabriel.ilic@gmail.com>
2018-12-08 20:45:33 +0100

    Created business logic for ForgottenPasswordActivity
    Implemented business logic for ForgottenPasswordActivity using Data Binding and following the MVVM architectural principle. Also, renamed LoginInfoViewModel to LoginViewModel and refactored code in LoginActivity accordingly.

--- app/src/main/java/hr/foi/air/sportloc/service/model/PrimitiveWrapperModel.java
+++ app/src/main/java/hr/foi/air/sportloc/service/model/PrimitiveWrapperModel.java
@@ -1,9 +1,18 @@
 package hr.foi.air.sportloc.service.model;
 
 public class PrimitiveWrapperModel {
+    private boolean success;
     private int userId;
 
     public PrimitiveWrapperModel() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
     }
 
     public int getUserId() {
