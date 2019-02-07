e95df43779de47caaa8cf661e2c2a6bff1ac7b62
vglozinic <vglozinic@gmail.com>
2019-02-07 16:03:20 +0100

    Adding OKHttp and removing module
    Creating timeout feature made by Gabriel and accidentally removed in previous merge

--- app/src/main/java/hr/foi/air/sportloc/service/caller/WebServiceCaller.java
+++ app/src/main/java/hr/foi/air/sportloc/service/caller/WebServiceCaller.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import hr.foi.air.sportloc.service.model.CommentModel;
 import hr.foi.air.sportloc.service.model.EventFilterModel;
@@ -18,6 +19,7 @@ import hr.foi.air.sportloc.service.rest.ApiInterface;
 import hr.foi.air.sportloc.service.serviceUtil.BooleanCallback;
 import hr.foi.air.sportloc.service.serviceUtil.DataUtil;
 import hr.foi.air.sportloc.service.serviceUtil.MessageCallback;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -31,10 +33,17 @@ public class WebServiceCaller {
     private static WebServiceCaller instance;
 
     private WebServiceCaller() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
+
         api = retrofit.create(ApiInterface.class);
     }
 
